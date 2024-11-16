4c66f2c5584077c4370c8084c0e52319518731c3
captainprice2002 <127361279+captainprice2002@users.noreply.github.com>
2024-11-16 23:47:53 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,14 +42,14 @@ resource "aws_security_group" "all_open" {
 
   ingress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
